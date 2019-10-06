7d13bab437d0b1a8420f49fc30a547181c1d5fc0
Anthony <anthony@quijano.net>
2019-10-06 09:10:09 -0700

    not sure what this is

---
+++ guessing_game/target/rls/debug/deps/guessing_game-20a1b8a77a9d89e3.d
@@ -0,0 +1,5 @@
+c:\Users\Antho\Rust-Projects\guessing_game\target\rls\debug\deps\guessing_game-20a1b8a77a9d89e3.rmeta: src\main.rs
+
+c:\Users\Antho\Rust-Projects\guessing_game\target\rls\debug\deps\guessing_game-20a1b8a77a9d89e3.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/rls/debug/deps/guessing_game-59643f891d42fe49.d
@@ -0,0 +1,5 @@
+c:\Users\Antho\Rust-Projects\guessing_game\target\rls\debug\deps\guessing_game-59643f891d42fe49.rmeta: src\main.rs
+
+c:\Users\Antho\Rust-Projects\guessing_game\target\rls\debug\deps\guessing_game-59643f891d42fe49.d: src\main.rs
+
+src\main.rs:
